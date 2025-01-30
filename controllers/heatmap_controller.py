df0842383a5762f0aef164d26bca77b6fd0fa2b8
Tarique Sani <tariquesani@gmail.com>
2025-01-30 14:33:39 +0530

    Added redirect to sync on error in JSON file reading

--- controllers/heatmap_controller.py
+++ controllers/heatmap_controller.py
@@ -1,6 +1,7 @@
 from bottle import template
 import json
 from services.map_service import generate_heatmap, generate_heatmap_with_time
+from bottle import redirect
 
 class HeatmapController:
     def index(self):
@@ -10,7 +11,8 @@ class HeatmapController:
                 activities = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             activities = []
-        
+            redirect('/sync')
+                    
         # Generate heatmap
         heatmap_html = generate_heatmap(activities)
         
@@ -25,6 +27,7 @@ class HeatmapController:
                 activities = json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             activities = []
+            redirect('/sync')
         
         # Generate heatmap
         heatmap_html = generate_heatmap_with_time(activities)
